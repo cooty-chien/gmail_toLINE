2a7173e9023973a56fc86b78c62b95eaa4ef3b09
cooty <cooty09312@gmail.com>
2026-08-19 12:50:57 +0800

    Add configuration settings for Gmail to LINE

---
+++ config.js
@@ -0,0 +1,7 @@
+// ===== config.js =====
+// Gmail to LINE 前端設定
+
+const CONFIG = {
+  LIFF_ID: '2011164374-MnsCQq2R',
+  GAS_URL: 'https://script.google.com/macros/s/AKfycbyOX1HfJGXMBQWWsNBes0WmZIgxiofSD2FwFyMI9ccshyn1gnXMa8kT0VRFsx8VgB-d/exec'
+};
